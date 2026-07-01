c1af5be3158bc549641cd8efdf5678ed900f56bf
Ryan Harris <ryan@synthesis.co.za>
2026-07-01 17:03:45 +0200

    added union find basic algo

---
+++ algomonster/advanced_data_structures/union_find.py
@@ -0,0 +1,31 @@
+class UnionFind:
+    # every element starts as its own root
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+
+    # find the root of x by walking parent pointers
+    def find(self, x: int) -> int:
+        if self.parent[x] == x:
+            return x
+        return self.find(self.parent[x])
+
+    # merge the clusters containing x and y
+    def union(self, x: int, y: int) -> None:
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return
+        self.parent[rx] = ry
+    
+    def get_parent(self):
+        return self.parent
+
+
+def driver():
+    n = 6
+    print("  node", list(range(n)))
+    dsu = UnionFind(n)
+    dsu.union(3, 1)
+    dsu.union(1, 0)
+    dsu.union(5, 4)
+    dsu.union(2, 0)
+    print("parent", dsu.get_parent())
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # import leetcode.easy.leetcode_268 as leetcode_easy
 # leetcode_easy.driver()
 
-import leetcode.medium.leetcode_7 as leetcode_medium
-leetcode_medium.driver()
+# import leetcode.medium.leetcode_7 as leetcode_medium
+# leetcode_medium.driver()
 
 # import leetcode.hard.leetcode_84 as leetcode_hard
 # leetcode_hard.driver()
+
+import algomonster.advanced_data_structures.union_find as uf
+uf.driver()
